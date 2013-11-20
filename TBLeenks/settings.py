51fadd90c3d67e018ce4aac3241091ece43bc7e6
creade <christopher.reade@gmail.com>
2013-11-19 23:14:20 -0800

    adding links app and default db connection

--- TBLeenks/settings.py
+++ TBLeenks/settings.py
@@ -55,12 +55,7 @@ WSGI_APPLICATION = 'TBLeenks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -82,7 +77,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default='postgres://localhost:5432/TBLeenks')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
