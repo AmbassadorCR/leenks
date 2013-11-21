03d01735f935e9ec7840c25dd9319e8b50266210
creade <christopher.reade@gmail.com>
2013-11-20 23:33:01 -0800

    js for delete and update

--- TBLeenks/static/js/LinksController.js
+++ TBLeenks/static/js/LinksController.js
@@ -1,13 +1,28 @@
-  function LinksListCtrl($scope, $http, $templateCache) {
+  function LinksListCtrl($scope, $http) {
     $scope.listLinks = function() {
-      $http({method: 'GET', url: '/links', cache: $templateCache}).
+      $http({method: 'GET', url: '/links'}).
           success(function(data, status, headers, config) {
               $scope.links = data.results;               
               $scope.view = './Partials/list.html'; 
           })
       }
+
+      $scope.delete = function(id){
+         $http({method: 'DELETE', url: '/links/'+id}).
+          success(function(data, status, headers, config) {
+              $scope.listLinks();
+          })
+      }
       
-      $scope.view = './Partials//list.html'; 
+
+      $scope.edit = function(link){
+         $http.put('/links/'+link.id, link).
+          success(function(data, status, headers, config) {
+              $scope.listLinks();
+          })
+      }
+
+      $scope.view = './Partials/list.html'; 
       $scope.listLinks();
   }
-  LinksListCtrl.$inject = ['$scope', '$http', '$templateCache'];
+  LinksListCtrl.$inject = ['$scope', '$http'];
